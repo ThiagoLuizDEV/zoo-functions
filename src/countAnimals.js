80631d8a373d9f33dc04a204a775c14ed814bc04
Thiago <thiago-470@outlook.com>
2022-10-03 22:33:00 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
   // seu código aqui
+  if (animal === undefined) {
+    const obj = {};
+
+    species.forEach((elemento) => {
+      obj[elemento.name] = elemento.residents.length;
+    });
+
+    return obj;
+  }
+  if ('sex' in animal) {
+    return species.find((element) => element.name === animal.specie)
+      .residents.filter((resident) => resident.sex === animal.sex).length;
+  }
+  return species.find((elemento) => elemento.name === animal.specie).residents.length;
 }
 
 module.exports = countAnimals;
