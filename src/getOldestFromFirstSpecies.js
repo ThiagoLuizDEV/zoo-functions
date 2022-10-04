b6606cb6876447589ea7896d2b726b07ac66b5bb
Thiago <thiago-470@outlook.com>
2022-10-03 21:59:33 -0300

    melhorando o requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -5,7 +5,8 @@ function getOldestFromFirstSpecies(id) {
   const colaborador = data.employees.find((employee) => employee.id === id);
   const primeiraEspecie = data.species.find((specie) => specie.id === colaborador
     .responsibleFor[0]);
-  const Velhos = primeiraEspecie.residents.reduce((acc, curr) => (acc.age > curr.age ? acc : curr));
+  const Velhos = primeiraEspecie.residents.reduce((acc, curr) =>
+    (acc.age > curr.age ? acc : curr));
   return Object.values(Velhos);
 }
 
